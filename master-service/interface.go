aeae6110b1c2154f57371243017c04b926409946
Dusan Skerget <skerdus@hilti.com>
2018-06-15 12:37:53 +0200

    Tracing and showing multiple spans implemented

--- master-service/interface.go
+++ master-service/interface.go
@@ -9,65 +9,14 @@ import (
 	jaegercfg "github.com/uber/jaeger-client-go/config"
 	jaegerlog "github.com/uber/jaeger-client-go/log"
 	"github.com/uber/jaeger-lib/metrics"
+	"github.com/opentracing/opentracing-go"
+	"time"
 )
 
 func main() {
 	log.Println("I am server")
 
 	// create Jaeger tracer
-	setupTracing()
-
-	http.HandleFunc("/", home)
-	http.HandleFunc("/call", wsCall)
-	//http.HandleFunc("/db-write", writeToDb)
-
-	if err := http.ListenAndServe(":8086", nil); err != nil {
-		panic(err)
-	}
-}
-
-func Sum(x, y int) (sum int){
-	return x + y
-}
-
-func wsCall(w http.ResponseWriter, r *http.Request) {
-
-	log.Println("Calling google")
-
-	var responseMessage string
-	response, err := http.Get("https://www.google.com")
-	if err != nil {
-		log.Printf("The HTTP request failed with error %s\n", err)
-		responseMessage = "The HTTP request failed with error"
-	} else {
-		data, _ := ioutil.ReadAll(response.Body)
-		responseMessage = string(data) + string(data)
-	}
-	log.Print("Response message is: ")
-	log.Print(responseMessage)
-}
-
-func home(w http.ResponseWriter, r *http.Request) {
-	message := "Home page of awesome interface"
-	w.Write([]byte(message))
-}
-
-func callDatabaseService(w http.ResponseWriter, r *http.Request) {
-
-	var responseMessage string
-	response, err := http.Get("http://localhost:8082/get")
-	if err != nil {
-		log.Printf("The HTTP request failed with error %s\n", err)
-		responseMessage = "The HTTP request failed with error"
-	} else {
-		data, _ := ioutil.ReadAll(response.Body)
-		responseMessage = string(data) + string(data)
-	}
-
-	w.Write([]byte(responseMessage))
-}
-
-func setupTracing() {
 	// Sample configuration for testing. Use constant sampling to sample every trace
 	// and enable LogSpan to log every span via configured Logger.
 	cfg := jaegercfg.Configuration{
@@ -88,7 +37,7 @@ func setupTracing() {
 
 	// Initialize tracer with a logger and a metrics factory
 	closer, err := cfg.InitGlobalTracer(
-		"serviceName",
+		"go-micro-interface",
 		jaegercfg.Logger(jLogger),
 		jaegercfg.Metrics(jMetricsFactory),
 	)
@@ -97,4 +46,78 @@ func setupTracing() {
 		return
 	}
 	defer closer.Close()
+
+	// Start server and expose endpoints
+	http.HandleFunc("/", home)
+	http.HandleFunc("/wscall", wsCall)
+	http.HandleFunc("/db-write", callDatabaseService)
+
+	if err := http.ListenAndServe(":8086", nil); err != nil {
+		panic(err)
+	}
+}
+
+// Function used to showcase how to do simple tests -> interface_test.go file
+func Sum(x, y int) (sum int){
+	return x + y
+}
+
+// Simple external GET REST call to google
+func wsCall(w http.ResponseWriter, r *http.Request) {
+
+	log.Println("Calling google")
+
+	var responseMessage string
+	response, err := http.Get("https://www.google.com")
+	if err != nil {
+		log.Printf("The HTTP request failed with error %s\n", err)
+		responseMessage = "The HTTP request failed with error"
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		responseMessage = string(data) + string(data)
+	}
+	log.Print("Response message is: ")
+	log.Print(responseMessage)
+}
+
+// Function showcases tracing functionality
+func home(w http.ResponseWriter, r *http.Request) {
+	tracer := opentracing.GlobalTracer()
+	span := tracer.StartSpan("render-home")
+
+	firstExecution(span)
+	secondExecution(span)
+
+	message := "Home page of awesome interface"
+	w.Write([]byte(message))
+
+	span.Finish()
+}
+
+func firstExecution(rootSpan opentracing.Span) {
+	span := rootSpan.Tracer().StartSpan("first-execution", opentracing.ChildOf(rootSpan.Context()))
+	time.Sleep(1 * time.Second)
+	span.Finish()
+}
+
+func secondExecution(rootSpan opentracing.Span) {
+	span := rootSpan.Tracer().StartSpan("second-execution", opentracing.ChildOf(rootSpan.Context()))
+	time.Sleep(3 * time.Second)
+	span.Finish()
+}
+
+// Function showcases call to another microservice
+func callDatabaseService(w http.ResponseWriter, r *http.Request) {
+
+	var responseMessage string
+	response, err := http.Get("http://localhost:8082/get")
+	if err != nil {
+		log.Printf("The HTTP request failed with error %s\n", err)
+		responseMessage = "The HTTP request failed with error"
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		responseMessage = string(data) + string(data)
+	}
+
+	w.Write([]byte(responseMessage))
 }
